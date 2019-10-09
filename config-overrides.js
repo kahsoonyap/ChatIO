027aba1ba19156e9c972f80487533bc1e87ca684
Gabby <emailgabby@gmail.com>
2019-10-09 15:38:48 -0400

    Basic frontend functionality

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+module.exports = function override(config, env) {
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /node_modules[/\\](?!react-native-gifted-chat|react-native-lightbox|react-native-parsed-text|expo-av)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        babelrc: false,
+        configFile: false,
+        presets: [
+          ['@babel/preset-env', { useBuiltIns: 'usage' }],
+          '@babel/preset-react',
+        ],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      },
+    },
+  })
+
+  return config
+}
