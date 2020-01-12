4301eccf2aa3111fbedf9581f71ca0f0607a1754
hankumin <chan4@stevens.edu>
2020-01-11 21:24:21 -0500

    Redoing frontend project

--- backend/server.js
+++ backend/server.js
@@ -1,72 +1,84 @@
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+
+const PORT = process.env.PORT||4001;
+
 const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const router = express.Router();
-var stream   = require('stream');
-const PORT = 4000;
 
-const cp = require("child_process");
-const { spawn } = require("child_process");
-
-const { onExit } = require("@rauschma/stringio");
-
-const file = "";
-
-app.use(cors());
-app.use(bodyParser.json());
-
-function runScript(){
-  return cp.spawn("python", ['-u',`helloworld.py`]);
-}
-
-let spawnChild;
-let death = false
-
-
-app.listen(PORT, function() {
-  console.log("Server is running on Port: " + PORT);
+router.get("/",(req,res)=>{
+  res.send({response: "Alive"}).status(200);
 });
 
-app.get("/express_backend", (req, res) => {
-  console.log("worked");
-  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
 
-app.get("/startup", async (req, res) => {
-  try {
-    spawnChild = runScript();
+server.listen(PORT);
+
+// const cp = require("child_process");
+// const { spawn } = require("child_process");
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// var stream   = require('stream');
+// function runScript(){
+//   return cp.spawn("python", ['-u',`helloworld.py`]);
+// }
+
+// app.listen(PORT, function() {
+//   console.log("Server is running on Port: " + PORT);
+// });
+
+// app.get("/express_backend", (req, res) => {
+//   console.log("worked");
+//   res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+// });
+
+// app.get("/startup", async (req, res) => {
+//   try {
+//     spawnChild = runScript();
     
-    spawnChild.stdout.once('data', function(data) { 
-      console.log(String.fromCharCode.apply(null,data))
-      res.send(data.toString());
-      death = false;
-    });
+//     spawnChild.stdout.once('data', function(data) { 
+//       console.log(String.fromCharCode.apply(null,data))
+//       res.send(data.toString());
+//       death = false;
+//     });
 
-    spawnChild.stdout.once('end', function() { 
-      death = true;
-    });
+//     spawnChild.stdout.once('end', function() { 
+//       death = true;
+//     });
 
-  } catch (e) {
-    console.log(e);
-  }
-});
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
 
-app.post("/send_message", async (req, res) => {
-  try {
-    if (!death){
-      spawnChild.stdin.write(req.body.send.text+"\n");      
+// app.post("/send_message", async (req, res) => {
+//   try {
+//     if (!death){
+//       spawnChild.stdin.write(req.body.send.text+"\n");      
 
-      spawnChild.stdout.once('data', function(data) { 
-        console.log(String.fromCharCode.apply(null,data))
-        res.send(data.toString());
-      });
-    }
-    else{
-      res.send({process:"false"});
-    }
-  } catch (e) {
-    console.log(e);
-    res.send("Couldnt send it my dude")
-  }
-});
+//       spawnChild.stdout.once('data', function(data) { 
+//         console.log(String.fromCharCode.apply(null,data))
+//         res.send(data.toString());
+//       });
+//     }
+//     else{
+//       res.send({process:"false"});
+//     }
+//   } catch (e) {
+//     console.log(e);
+//     res.send("Couldnt send it my dude")
+//   }
+// });
+
