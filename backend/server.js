e6c3dbb4d93e0d97b05c11ca999f4ea8b9edb51c
JHobbie <jaredhobbie1@gmail.com>
2020-04-20 21:42:38 -0400

    Check if youre on develop first

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(router);
 
 router.get("/",(req,res)=>{
